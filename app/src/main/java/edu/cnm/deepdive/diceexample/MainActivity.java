209b24aa06f97bb3aeebbd2821ea33a17f970e43
che money <cmelendez15@cnm.edu>
2023-10-11 12:01:46 -0500

    initial commit

---
+++ app/src/main/java/edu/cnm/deepdive/diceexample/MainActivity.java
@@ -0,0 +1,19 @@
+package edu.cnm.deepdive.diceexample;
+
+import android.util.Log;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    OnClickListener listener = (v) -> Log.d(getClass().getSimpleName(), ((Button) v).getText() + "");
+    findViewById(R.id.first).setOnClickListener(listener);
+    findViewById(R.id.second).setOnClickListener(listener);
+  }
+}
