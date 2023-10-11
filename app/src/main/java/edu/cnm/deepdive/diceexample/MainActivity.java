bbe4fad7aaa99cbe40adf7f980a415c587cf12ac
che money <cmelendez15@cnm.edu>
2023-10-11 16:06:10 -0500

    second  commit

--- app/src/main/java/edu/cnm/deepdive/diceexample/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/diceexample/MainActivity.java
@@ -5,15 +5,36 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.lifecycle.ViewModelProvider;
+import edu.cnm.deepdive.diceexample.databinding.ActivityMainBinding;
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+  private ActivityMainBinding binding;
+  private DiceRollViewModel viewmodel;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     OnClickListener listener = (v) -> Log.d(getClass().getSimpleName(), ((Button) v).getText() + "");
-    findViewById(R.id.first).setOnClickListener(listener);
-    findViewById(R.id.second).setOnClickListener(listener);
+   binding.first.setOnClickListener(listener);
+    binding.second.setOnClickListener(listener);
+    binding.third.setOnClickListener((v) -> viewmodel.rollDice(20, 9));
+  }
+
+  private void setupViewmodel () {
+    viewmodel = new ViewModelProvider(this).get(DiceRollViewModel.class);
+    viewmodel.getDiceRoll().observe(this, (rolls) -> Log.d(getClass().getSimpleName(),
+        Arrays.toString(rolls)));
+    viewmodel.getThrowable()
+        .observe(this, (throwable -> {
+          if (throwable != null) {
+            Log.e(getClass().getSimpleName(), throwable.getMessage(), throwable);
+          }
+        }));
   }
 }
